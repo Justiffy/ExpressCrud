eef658ef5450fdecad761e449164d59a2ed764af
Listar <shnap2010@yandex.ru>
2019-06-06 15:38:20 +0500

    error handle fix

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,12 +1,5 @@
 const Product = require('../models/product.model');
 
-const setStatusError = (err, res, next) => {
-  if (err.name === 'ValidationError') {
-    return res.status(422).send(err);
-  }
-  return next(err);
-};
-
 exports.test = (req, res) => {
   res.send('teeeest route');
 };
@@ -18,28 +11,48 @@ exports.productCreate = (req, res, next) => {
   });
 
   product.save((err) => {
-    if (err) setStatusError(err, res, next);
+    if (err) {
+      if (err.name === 'ValidationError') {
+        return res.status(422).send(err);
+      }
+      return next(err);
+    }
     res.send('Product Created successfully');
   });
 };
 
 exports.productDetails = (req, res, next) => {
   Product.findById(req.params.id, (err, product) => {
-    if (err) setStatusError(err, res, next);
+    if (err) {
+      if (err.name === 'ValidationError') {
+        return res.status(422).send(err);
+      }
+      return next(err);
+    }
     res.send(product);
   });
 };
 
 exports.productUpdate = (req, res, next) => {
   Product.findByIdAndUpdate(req.params.id, { $set: req.body }, (err) => {
-    if (err) setStatusError(err, res, next);
+    if (err) {
+      if (err.name === 'ValidationError') {
+        return res.status(422).send(err);
+      }
+      return next(err);
+    }
     res.send('Product udpated.');
   });
 };
 
 exports.productDelete = (req, res, next) => {
   Product.findByIdAndRemove(req.params.id, (err) => {
-    if (err) setStatusError(err, res, next);
+    if (err) {
+      if (err.name === 'ValidationError') {
+        return res.status(422).send(err);
+      }
+      return next(err);
+    }
     res.send('Deleted successfully!');
   });
 };
